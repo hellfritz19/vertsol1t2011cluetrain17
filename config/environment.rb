a488a5cdc65e69aea1c2909b3cb0a9d46eb7c5c7
Amit Kumar <hellfritz19@yahoo.com>
2011-07-02 00:38:23 +0800

    my first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Vertsol1t2011cluetrain17::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vertsol1t2011cluetrain17::Application.config.secret_token = '3dab734bb2b40893829c2f081bda619feef29aa060e55929d1b8a8322b97f002b7505f8a8590d479efd8007a0d9397b4e4e61d9d7ad73dabb352e7ce73a849e2'
